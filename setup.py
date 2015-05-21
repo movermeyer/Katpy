f09b9994a2b9cb4fe6124019bca6efc6ccb9ad22
Sachin Kelkar <sachinkel19@gmail.com>
2015-05-22 00:34:39 +0530

    Fixed a minor indentation bug

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='katpy',
-      version='1.0.1',
+      version='1.0.2',
       description='An unofficial Python API for http://kickass.to/',
       url='https://github.com/s4chin/Katpy',
       author='Sachin Kelkar',
