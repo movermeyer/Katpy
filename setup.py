8254fa5fb8f9ba1b4b92635ea0b5fd8d431340e3
Sachin Kelkar <sachinkel19@gmail.com>
2015-05-22 00:19:54 +0530

    Update setup.py for new release

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='katpy',
-      version='1.0.0',
+      version='1.0.1',
       description='An unofficial Python API for http://kickass.to/',
       url='https://github.com/s4chin/Katpy',
       author='Sachin Kelkar',
